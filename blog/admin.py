e3bb79ee4f0ffb90969f3b526156694e71170d47
@britodfbr <britodfbr@gmail.com>
2020-09-20 15:02:59 -0300

    register alterado do generico para Personalizado; lista de exibição do conteúdo

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = 'title', 'slug', 'author', 'published', 'created', 'changed', 'status',
+
+
