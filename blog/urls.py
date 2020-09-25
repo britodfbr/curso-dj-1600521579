c68256e4a287f809d622bcf09d4812828194075e
@britodfbr <britodfbr@gmail.com>
2020-09-25 14:31:29 -0300

    import e configure blog views

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,10 @@
 # /bin/env python
 # -*- encode: utf-8 -*-
 __author__ = '@britodfbr'
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.BlogListView.as_view(), name='home')
+]
