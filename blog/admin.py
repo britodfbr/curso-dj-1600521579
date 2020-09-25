6646d608afdf173f50158d182d221ef0dbdc748a
@britodfbr <britodfbr@gmail.com>
2020-09-25 10:55:00 -0300

    Post.objects.bulk_create: cria objetos a partir de uma lista

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = 'title', 'slug', 'author', 'published', 'created', 'changed', 'status',
@@ -10,5 +11,19 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = 'title', 'content',
     prepopulated_fields = {'slug': ('title',)}
     date_hierarchy = 'published'
+    raw_id_fields = 'author',
 
 
+if __name__ == '__main__':
+    pass
+    """
+    from django.contrib.auth.models import User
+    user = User.objects.get(username='admin')
+    p = 'Testando o shell do Django'
+    post = Post(title=p, slug=p.lower().replace(' ', '_'), content=p.upper(), author=user)
+    frase = 'Criado via bulk {}'
+    posts = [Post(title=frase.format(x), slug=frase.format(x).lower().replace(' ', '_'),
+                  content=frase.format(x).upper(), author=user)
+             for x in range(10)]
+    Post.objects.bulk_create(posts)
+    """
