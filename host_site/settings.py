176bc1e0614b2291eb95dba13fb01af18f0e079c
@britodfbr <britodfbr@gmail.com>
2020-09-25 12:23:26 -0300

    Configuração de diretório para arquivos estáticos

--- host_site/settings.py
+++ host_site/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'host_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    'statics'
+]
