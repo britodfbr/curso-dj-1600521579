bd975a664e23a1a082ce1095de27db80090f1e3e
@britodfbr <britodfbr@gmail.com>
2020-09-26 09:58:24 -0300

    UpdateView (pk)

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import (ListView, DetailView)
+from django.views.generic import (ListView, DetailView, UpdateView)
 from django.views.generic.edit import CreateView
 from .models import Post
 
@@ -22,3 +22,10 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'blog/post_new.html'
     fields = '__all__'
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    fields = 'author', 'title', 'content',
+
