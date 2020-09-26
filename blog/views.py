14b515f21398078a2bec880620c066e52ead9d9b
@britodfbr <britodfbr@gmail.com>
2020-09-26 13:46:41 -0300

    Mensagens de Feedback para DeleteView, UpdateView, CreateView

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import (ListView, DetailView, UpdateView, DeleteView)
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
@@ -18,19 +20,43 @@ class BlogDetailView(DetailView):
     # context_object_name = 'custom'   # default: object
 
 
-class BlogCreateView(CreateView):
+class BlogCreateView(SuccessMessageMixin, CreateView):
     model = Post
     template_name = 'blog/post_new.html'
     fields = '__all__'
+    success_message = '%(field)s: Criado com Sucesso!'
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            field=self.object.title,
+        )
 
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(SuccessMessageMixin, UpdateView):
     model = Post
     template_name = 'blog/post_edit.html'
     fields = 'author', 'title', 'content',
+    success_message = '%(field)s: Alterado com Sucesso!'
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            field=self.object.title,
+        )
 
 
-class BlogDeleteView(DeleteView):
+class BlogDeleteView(SuccessMessageMixin, DeleteView):
     model = Post
     template_name = 'blog/post_delete.html'
     success_url = reverse_lazy('home')
+    success_message = 'Removido com Sucesso!'   #'%(field)s: Removido com Sucesso!'
+
+    # def get_success_message(self, cleaned_data):
+    #     return self.success_message % dict(
+    #         cleaned_data,
+    #         field=self.object.title,
+    #     )
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(BlogDeleteView, self).delete(request, *args, **kwargs)
