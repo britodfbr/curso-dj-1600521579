28f5740ee7e6b7799655a47ff4eeaa0802b1894c
@britodfbr <britodfbr@gmail.com>
2020-10-06 22:36:37 -0300

    Acrescentado campos para completar Formulário PostForm

--- blog/forms.py
+++ blog/forms.py
@@ -12,4 +12,4 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = 'title', 'content', 'status',
+        fields = 'title', 'content', 'category', 'image', 'status',
