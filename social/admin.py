741688902551853ffd41c1d687d20072acb6f91d
@britodfbr <britodfbr@gmail.com>
2020-10-08 09:15:48 -0300

    Criado modelo para o modulo social

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Link
+
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    readonly_fields = 'created', 'changed',
+    list_display = 'key', 'created', 'changed',
--- social/models.py
+++ social/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Link(models.Model):
+    key = models.SlugField(verbose_name="Identificação Rede", max_length=100, unique=True)
+    descrition = models.CharField(verbose_name='Descrição', max_length=100)
+    url = models.URLField(max_length=200, null=False, blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    changed = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Link"
+        verbose_name_plural = "Links"
+        ordering = ['key']
+
+    def __str__(self):
+        return self.key
